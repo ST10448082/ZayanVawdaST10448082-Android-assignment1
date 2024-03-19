d45e6e04790d2bc70927a043228474e67e4699f8
ST10448082 <ST10448082@vcconnect.edu>
2024-03-19 15:34:38 +0200

    finished project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+} val textInput =EditTextNumberId<EditText>(R.id.editTextNumber)
+clickMeButton?setOnClicklistener{B
+    var message:String
+    if(textInput)=0&&textInput(=10){
+        message="Welcome"
+
+        val editfield
